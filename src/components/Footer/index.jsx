7d613d0833ca00127b8b55df8db42cbe433bed77
WParsec <91538926+WParsec@users.noreply.github.com>
2023-03-09 14:59:09 +0100

    reviews section

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -2,20 +2,25 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 import styles from "./Footer.module.scss";
+import "../../scss/main.scss"
 
 export function Footer() {
     return (
         <footer>
-            <div className={styles.container}>
-                <div className="footer-logo"></div>
-                <div className="footer-links">
-                    <ul>
-                        <li>
-                            <Link to="/">Home</Link>
-                        </li>
-                    </ul>
+            <section>
+                <div className="container">
+                    <div>
+                        <ul className={styles.footer_links}>
+                            <li>
+                                <Link to="/">Home</Link>
+                            </li>
+                            <li>
+                                <Link to="contact">Contact</Link>
+                            </li>
+                        </ul>
+                    </div>
                 </div>
-            </div>
+            </section>
         </footer>
     );
 }
--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -11,8 +11,9 @@ export function Product() {
     let { id } = useParams();
     let url = `https://api.noroff.dev/api/v1/online-shop/${id}`;
     const [product, isLoading, isError] = useAPI(url, {});
-    const { title, description, price, discountedPrice, imageUrl, rating, reviews } = product;
-    console.log(reviews)
+    const { title, description, price, discountedPrice, imageUrl, rating, reviews} = product;
+
+    
     
     let discount;
 
@@ -51,18 +52,24 @@ export function Product() {
                         </div>
                     </div>
                     <section>
-                    <h2>Reviews</h2>
-                        {reviews ? <div className={styles.reviews_wrap}>
-                            {reviews.map((review) => {
-                            return (
-                                <div className={styles.review_card}>
-                                    <p>{review.username}</p>
-                                    <p>{review.description}</p>
-                                    <p>Rating: {review.rating}</p>
+                        <h2 className={styles.section_h2}>Reviews</h2>
+                        {reviews && reviews.length > 0 ? (
+                            <div className={styles.reviews_container}>
+                                <div className={styles.reviews_wrap}>
+                                    {reviews.map((review) => {
+                                        return (
+                                            <div className={styles.review_card} key={review.id}>
+                                                <p>{review.username}</p>
+                                                <p>{review.description}</p>
+                                                <p>Rating: {review.rating}</p>
+                                            </div>
+                                        );
+                                    })}
                                 </div>
-                            );
-                            })}
-                        </div> : ""}
+                            </div>
+                        ) : (
+                            <p className={styles.no_reviews}>No reviews yet</p>
+                        )}
                     </section>
                 </div>
             </section>
