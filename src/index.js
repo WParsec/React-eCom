8ca15c0af2cc351db88ff4bb8090d99dbf1d61aa
WParsec <91538926+WParsec@users.noreply.github.com>
2023-03-24 13:39:43 +0100

    adds persist

--- src/index.js
+++ src/index.js
@@ -1,18 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import { store } from './store';
+import { store, persistor } from './store';
 import { Provider } from 'react-redux';
 import { HashRouter } from 'react-router-dom';
+import { PersistGate } from 'redux-persist/integration/react'; // PersistGate delays the rendering of your app's UI until your persisted state has been retrieved and saved to redux.
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Provider store={store}>
-    <HashRouter>
-      <App />
-    </HashRouter>
+    <PersistGate loading={null} persistor={persistor}>
+      <HashRouter>
+        <App />
+      </HashRouter>
+    </PersistGate>
   </Provider>
 );
-
-// Hydrogen is a library for storing and retrieving data from the browser's local storage.
--- src/store.jsx
+++ src/store.jsx
@@ -1,10 +1,25 @@
 import { configureStore } from '@reduxjs/toolkit';
 
+// Redux Persist
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+
 // Import the reducer from the slice file. It is a default export so we can name it whatever we want.
 import cartReducer from './cartSlice';
 
+// Create a persist configuration object
+const persistConfig = {
+    key: 'root',
+    storage,
+  };
+
+// Wrap the cartReducer with persistReducer and pass in the persistConfig object
+const persistedCartReducer = persistReducer(persistConfig, cartReducer);
+
 export const store = configureStore({
     reducer: {
-        cart: cartReducer,
+        cart: persistedCartReducer,
     },
 });
+
+export const persistor = persistStore(store);
